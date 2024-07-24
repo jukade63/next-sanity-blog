7dcb656da83a9760554c51f1b500ee30df0f64b7
jukade63 <jukade63@gmail.com>
2024-07-24 22:42:34 +0700

    change procution link

--- app/utils/constants.ts
+++ app/utils/constants.ts
@@ -1,4 +1,4 @@
 export const DEFAULT_IMAGE_URL = "https://images.unsplash.com/photo-1551288049-bebda4e38f71?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
 
 
-export const HOSTNAME = process.env.NODE_ENV === 'production' ? "https://next-cms-blog.netlify.app" : "http://localhost:3000"
+export const HOSTNAME = process.env.NODE_ENV === 'production' ? "https://next-sanity-blog-amber.vercel.app" : "http://localhost:3000"
